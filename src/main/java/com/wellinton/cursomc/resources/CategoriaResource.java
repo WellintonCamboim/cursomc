587aff88e4892029f687c7b17dcbb294ad6309a5
WellintonCamboim <wellinton.camboim@gmail.com>
2019-03-20 22:46:18 -0300

    REST testado

--- src/main/java/com/wellinton/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/wellinton/cursomc/resources/CategoriaResource.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value="categorias")
 public class CategoriaResource {
-	
+	//Testando o REST
 	@RequestMapping(method=RequestMethod.GET)
 	public String listar() {
 		return "REST está funcionando!";
