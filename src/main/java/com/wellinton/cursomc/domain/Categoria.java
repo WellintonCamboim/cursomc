6c860db6ed94b6fb8bbbde9cb0145f8549017da6
WellintonCamboim <wellinton.camboim@gmail.com>
2019-03-24 23:06:47 -0300

    Banco de dados H2 e criação automatica da base de dados

--- src/main/java/com/wellinton/cursomc/domain/Categoria.java
+++ src/main/java/com/wellinton/cursomc/domain/Categoria.java
@@ -2,9 +2,18 @@ package com.wellinton.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity //Indica que a presente class será uma entidade do JPA
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY) //Para definir a estratégia de geração automatica 
+	//dos Ids das minhas categorias 
 	private Integer id;
 	private String nome;
 	
