ebda3dae74a3cf8be229e779847176e85ff74bc9
WellintonCamboim <wellinton.camboim@gmail.com>
2019-04-26 17:26:37 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/wellinton/cursomc/CursomcApplication.java
+++ src/main/java/com/wellinton/cursomc/CursomcApplication.java
@@ -81,6 +81,11 @@ public class CursomcApplication implements CommandLineRunner {
 
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
+		Categoria cat4 = new Categoria(null, "Eletrônicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Perfumaria");	
 
 		// Instanciando produto e categoria = Criando os objetos
 
@@ -107,7 +112,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
 		// categoriaRepository.saveAll --> irá salvar as categorias do banco
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		// Para salvar produto
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
