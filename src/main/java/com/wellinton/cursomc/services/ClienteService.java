5ec14895850ceaa8212cc460e73cd5dac4ea91a8
WellintonCamboim <wellinton.camboim@gmail.com>
2019-04-29 17:15:23 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/wellinton/cursomc/services/ClienteService.java
+++ src/main/java/com/wellinton/cursomc/services/ClienteService.java
@@ -1,12 +1,20 @@
 package com.wellinton.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.wellinton.cursomc.domain.Cliente;
+import com.wellinton.cursomc.domain.Cliente;
+import com.wellinton.cursomc.dto.ClienteDTO;
 import com.wellinton.cursomc.repositories.ClienteRepository;
+import com.wellinton.cursomc.services.exception.DataIntegrityException;
 import com.wellinton.cursomc.services.exception.ObjectNotFoundException;
 
 // Camada de serviços de Cliente
@@ -22,4 +30,38 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
+
+	// Aula - S3-32
+	public Cliente update(Cliente obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	// Aula - S3-33 - DELETE
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+	}
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	//Aula - S3-35
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
+		return repo.findAll(pageRequest);
+		
+	}
+	//Aula - S3-36 - A partir de um DTO o sistema irá construir um objeto Cliente
+	//Método auxiliar que instancia uma Cliente a partir de um DTO 
+	public Cliente fromDTO(ClienteDTO objDto) {
+		throw new UnsupportedOperationException();
+		//return new Cliente(objDto.getId(), objDto.getNome());
+	}
+
+	
 }
